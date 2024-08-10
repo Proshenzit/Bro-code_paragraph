f1085481c35c3b72b91de7d30241c3531817baea
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-08-10 04:52:59 -0700

    Add files via upload

---
+++ bro_code_paragraph.py
@@ -0,0 +1,11 @@
+djective1 = input("Enter  an Adjective : ")
+noun = input("Enter  a noun ")
+adjective2 = input("Enter  an Adjective : ")
+verb = input("Enter a verb ")
+
+adjective3 = input("Enter  an Adjective : ")
+
+print(f"Today  i   went  to   a {adjective1}zoo.")
+print(f"in exhabit ,i saw {noun}")
+print(f" {noun} was {adjective2 } and {verb}ing ")
+print(f"i was {adjective3}")
